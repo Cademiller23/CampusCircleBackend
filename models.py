9bc348819d5f3dc0f79a4a2fb987ffb38172f63c
Cademiller23 <97142161+Cademiller23@users.noreply.github.com>
2024-06-25 17:39:57 -0700

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy # Import SQLAlchemy for database Interac
 from uuid import uuid4  # Import the uuid module for generating unique IDs
 from database import db # Import the database instance from your database
 from sqlalchemy.sql import func # import for timestamp
-
+from flask_migrate import Migrate
 # Function to generate a unqiue hexidecimal ID 
 def get_uuid():
     return uuid4().hex 
@@ -15,7 +15,7 @@ class User(db.Model):
     password = db.Column(db.Text, nullable=False) # Non-nullable password column
     profile_picture = db.Column(db.String(255)) # Store profile picture URL
     username = db.Column(db.String(50), unique=True, nullable=False) # Unique username 
-
+    
 # Post Model representing users posts
 class Post(db.Model):
     __tablename__ = "posts" # Specifies table name
@@ -24,6 +24,15 @@ class Post(db.Model):
     content_type = db.Column(db.String(10), nullable=False) # Image or Video have to program with setTimeout in React to make it so holding camera inputs Video and clicking is Image
     content_url = db.Column(db.String(255), nullable=False) # URL to store content
     timestamp = db.Column(db.DateTime(timezone=True), default=func.now())
+    category = db.Column(db.String)
+    user = db.relationship("User", backref="posts")
+    # upVotes = db.Column(db.integer())
+# Comment class 
+    # comment id 
+    # Foreign Key - Post, User 
+    # Comment text 
+    # Timestamp 
+
 
     # Creates the relationship between user and post (1:M) relationship 
-    user = db.relationship("User", backref="posts")
+    
