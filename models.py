f7c3fe6183338a9648b4eb73011fb943942f3029
Cademiller23 <97142161+Cademiller23@users.noreply.github.com>
2024-08-06 11:15:12 -0700

    Update models.py
    Added Database Models

--- models.py
+++ models.py
@@ -28,8 +28,9 @@ class Post(db.Model):
     content_url = db.Column(db.String(255), nullable=False) # URL to store content
     timestamp = db.Column(db.DateTime(timezone=True), default=func.now())
     category = db.Column(db.String)
+    like_count = db.Column(db.Integer, nullable=False, default=0) # New like_count
     user = db.relationship("User", backref="posts")
-    # upVotes = db.Column(db.integer())
+    
 
 class Comment(db.Model):
     __tablename__ = "comments"
@@ -41,7 +42,35 @@ class Comment(db.Model):
     post = db.relationship("Post", backref="comments")
     user = db.relationship("User", backref="comments")
 
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(32), db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.String(32), db.ForeignKey('posts.id'), nullable=False)
+    is_like = db.Column(db.Boolean, nullable=False) # True for likes, False for dislikes
+    user = db.relationship('User', backref=db.backref('likes', lazy=True))
+    post = db.relationship('Post', backref=db.backref('likes', lazy=True))
 
-    # Creates the relationship between user and post (1:M) relationship 
+# Poll model
+class Poll(db.Model):
+    id = db.Column(db.String(32), primary_key=True, default=get_uuid)
+    title = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.String(32), db.ForeignKey('users.id'), nullable=False)
+    options = db.relationship('PollOption', backref='poll', lazy=True, cascade="all, delete-orphan")
+    user = db.relationship('User', backref='polls')  # Add this line to define the relationship
+# Poll Option model
+class PollOption(db.Model):
+    id = db.Column(db.String(32), primary_key=True, default=get_uuid)
+    poll_id = db.Column(db.String(32), db.ForeignKey('poll.id'), nullable=False)
+    text = db.Column(db.String(255), nullable=False)
+    vote_count = db.Column(db.Integer, default=0)
+
+# Poll Vote model
+class PollVote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(32), db.ForeignKey('users.id'), nullable=False)
+    poll_id = db.Column(db.String(32), db.ForeignKey('poll.id'), nullable=False)
+    option_id = db.Column(db.String(32), db.ForeignKey('poll_option.id'), nullable=False)
+    
+
     
 
